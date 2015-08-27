309c0ca5721ef7f22a756aeb17d0d7350290acd5
Terry G Lorber <tgl@rideside.net>
2015-08-26 23:40:34 -0400

    This fixes #2 by using fkeys from redis to construct AWS S3 URLs.

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 class App < Sinatra::Base
 
   get '/' do
-    files = Dir.glob('./public/images/*.jpg')
-    files.sort!
-    @files = files.map{|x| File.basename(x)}
+    assets = Asset.all(:s3_fkey.not => nil, :order => [:id.asc])
+    @urls = assets.map{|x| "https://s3.amazonaws.com/haze-assets/#{x.s3_fkey}"}
     haml :index
   end
 
