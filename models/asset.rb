a111907f24b3ec720b9d4ac32fa89f4559c4b360
Terry G Lorber <tgl@rideside.net>
2015-09-01 00:40:29 -0400

    TYPO

--- models/asset.rb
+++ models/asset.rb
@@ -20,7 +20,7 @@ class Asset
     return fkey
   end
 
-  before :delete do 
+  before :destroy do 
     puts "INFO: Asset {self.id} exists with S3 #{self.s3_fkey}? #{AWS::S3::S3Object.exists?(self.s3_fkey, ENV['S3_BUCKET_NAME'])}"
     AWS::S3::S3Object.delete(self.s3_fkey, ENV['S3_BUCKET_NAME'])
   end
