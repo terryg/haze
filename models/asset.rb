b21f35e993a4d650dc971773bd515307b11110ff
Terry G Lorber <tgl@rideside.net>
2015-08-31 23:32:20 -0400

    More debugging output

--- models/asset.rb
+++ models/asset.rb
@@ -21,6 +21,7 @@ class Asset
   end
 
   def destroy
+    puts "INFO: Asset {self.id} exists with S3 #{self.s3_fkey}? #{AWS::S3::S3Object.exists?(self.s3_fkey, ENV['S3_BUCKET_NAME'])}"
     AWS::S3::S3Object.delete(self.s3_fkey, ENV['S3_BUCKET_NAME'])
     super
   end
