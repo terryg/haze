2606afee1eff0c072f779807c01938935e98fc29
Terry G Lorber <tgl@rideside.net>
2016-06-09 00:16:19 -0400

    LIFO

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ class App < Sinatra::Base
   get '/' do
     @asset = Asset.first(:s3_fkey.not => nil,
                          :type => "GIF", 
-                         :order => [:created_at.asc])
+                         :order => [:created_at.desc])
     haml :index
   end
 
